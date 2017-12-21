06434465e00e3ec070c6f1198d8d1ea177c3052d
Radosław Miernik <radekmie@gmail.com>
2017-12-21 10:41:01 +0100

    Added meteor/accounts-webapp.

---
+++ meteor_webapp_vx.x.x.js
@@ -0,0 +1,84 @@
+// TODO: This should be extracted.
+import type {Server} from 'http';
+
+class npm$connect$IncomingMessage extends http$IncomingMessage {
+  originalUrl: string;
+}
+
+class npm$connect$ServerResponse extends http$ServerResponse {}
+
+type npm$connect$handle =
+  | ((
+      req: npm$connect$IncomingMessage,
+      res: npm$connect$ServerResponse,
+      next: (?Error) => void
+    ) => void)
+  | ((
+      error: Error,
+      req: npm$connect$IncomingMessage,
+      res: npm$connect$ServerResponse,
+      next: (?Error) => void
+    ) => void);
+
+declare class npm$connect {
+  route: string;
+  stack: {handle: npm$connect$handle, route: string}[];
+  use(route: string, handle: npm$connect$handle): npm$connect;
+}
+
+declare class Meteor$WebApp {
+  addHtmlAttributeHook(fn: (Meteor$WebApp$Request) => {[string]: string}): void;
+  connectApp: npm$connect;
+  httpServer: Server;
+  rawConnectHandlers: npm$connect;
+}
+
+declare type Meteor$WebApp$Request = {
+  browser: {
+    major: number,
+    minor: number,
+    name: string,
+    patch: number
+  },
+  cookies: {[string]: string},
+  dynamicBody: string,
+  dynamicHead: string,
+  headers: {[string]: string},
+  url: {
+    pathname: string
+  }
+};
+
+declare class Meteor$WebAppInternals {
+  registerBoilerplateDataCallback(
+    key: string,
+    fn: (
+      Meteor$WebApp$Request,
+      {
+        dynamicBody: string,
+        dynamicHead: string,
+        headers?: {[string]: string},
+        statusCode?: number,
+        [string]: string
+      },
+      string
+    ) => boolean
+  ): void;
+  staticFiles: {
+    [string]: {
+      absolutePath: string,
+      cacheable: boolean,
+      content?: string,
+      hash?: string,
+      sourceMapUrl?: string,
+      type?: string
+    }
+  };
+}
+
+declare module 'meteor/webapp' {
+  declare module.exports: {
+    WebApp: Meteor$WebApp,
+    WebAppInternals: Meteor$WebAppInternals
+  };
+}
